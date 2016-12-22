3a13ddefa275421940f1bbc03fefe83b678ac079
Daniel <nieltg@users.noreply.github.com>
2016-12-22 15:21:32 +0700

    route: Add route to Profile, Search, Settings

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,18 @@ Route::get('/', 'HomeController@index');
 Route::get('auth/login', 'Auth\LoginController@showLoginForm');
 Route::post('auth/login', 'Auth\LoginController@login');
 Route::get('auth/logout', 'Auth\LoginController@logout');
+
+Route::get('profile', 'ProfileController@index');
+Route::get('profile/edit', 'ProfileController@edit');
+Route::post('profile/edit', 'ProfileController@update');
+Route::get('profile/confirm', 'ProfileController@confirm');
+
+Route::get('profile/{nim}', 'ProfileController@show');
+Route::get('profile/{nim}/edit', 'ProfileController@edit');
+Route::post('profile/{nim}/edit', 'ProfileController@update');
+Route::get('profile/{nim}/confirm', 'ProfileController@confirm');
+
+Route::get('search', 'SearchController@index');
+
+Route::get('settings', 'SettingsController@index');
+Route::post('settings', 'SettingsController@update');
